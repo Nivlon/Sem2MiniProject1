713d66b5b86a6c89b2a4c55bb153d3e0d40f725e
Fabian Demeter <fabiandemeter22@gmail.com>
2019-04-27 15:05:07 +0200

    The App That Goes Click

---
+++ MySecondApplication/app/src/main/java/com/example/mysecondapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.mysecondapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public int clickCounter=0;
+    public String counterString="0";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        TextView textView = findViewById(R.id.textView);
+        clickCounter++;
+        counterString= Integer.toString(clickCounter);
+        textView.setText(counterString);
+}
+}
