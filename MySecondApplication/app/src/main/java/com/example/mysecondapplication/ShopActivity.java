a3fbf634db74bf68f75a0b5cd997b19e3356e343
Fabian Demeter <fabiandemeter22@gmail.com>
2019-04-27 16:14:05 +0200

    Yeah, okay

--- MySecondApplication/app/src/main/java/com/example/mysecondapplication/ShopActivity.java
+++ MySecondApplication/app/src/main/java/com/example/mysecondapplication/ShopActivity.java
@@ -3,11 +3,11 @@ package com.example.mysecondapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TestActivity extends AppCompatActivity {
+public class ShopActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_shop);
     }
 }
