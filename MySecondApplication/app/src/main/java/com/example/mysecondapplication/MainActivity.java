94b72a2c5fda7cb722f416bcb0a4efe12a05a43f
Marcus <m.h.otterstreom@gmail.com>
2019-04-27 19:34:55 +0200

    Shop and Power Up
    Shop now has an upgrade
    - Power up Clicks

--- MySecondApplication/app/src/main/java/com/example/mysecondapplication/MainActivity.java
+++ MySecondApplication/app/src/main/java/com/example/mysecondapplication/MainActivity.java
@@ -8,23 +8,30 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public int clickCounter=0;
-    public String counterString="0";
+    public static int clickPower = 1;
+    public static int clickPointCounter = 0;
+    public static String clickPointString = "0";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.clickPoints);
+        clickPointString= Integer.toString(clickPointCounter);
+        textView.setText(clickPointString);
     }
 
     public void sendMessage(View view) {
-        TextView textView = findViewById(R.id.textView);
-        clickCounter++;
-        counterString= Integer.toString(clickCounter);
-        textView.setText(counterString);
+        TextView textView = findViewById(R.id.clickPoints);
+        clickPointCounter+=clickPower;
+        clickPointString= Integer.toString(clickPointCounter);
+        textView.setText(clickPointString);
 }
     public void goToShop(View view) {
         Intent intent = new Intent(this, ShopActivity.class);
         startActivity(intent);
     }
+
+    public static void refreshValues() {}
 }
--- MySecondApplication/app/src/main/java/com/example/mysecondapplication/ShopActivity.java
+++ MySecondApplication/app/src/main/java/com/example/mysecondapplication/ShopActivity.java
@@ -3,13 +3,38 @@ package com.example.mysecondapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ShopActivity extends AppCompatActivity {
+    public static String clickPowerString = "0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+        TextView textview = findViewById(R.id.clickPowerDisplay);
+        clickPowerString = Integer.toString(MainActivity.clickPower*10);
+        textview.setText(clickPowerString);
+        TextView textView = findViewById(R.id.clickPoints);
+        MainActivity.clickPointString= Integer.toString(MainActivity.clickPointCounter);
+        textView.setText(MainActivity.clickPointString);
+    }
+
+
+    public void clickPowerUp(View view){
+        if (MainActivity.clickPointCounter >= (10*MainActivity.clickPower)) {
+            MainActivity.clickPointCounter -=(10 * MainActivity.clickPower);
+            MainActivity.clickPower++;
+
+            TextView textview = findViewById(R.id.clickPowerDisplay);
+            clickPowerString = Integer.toString(MainActivity.clickPower*10);
+            textview.setText(clickPowerString);
+            TextView textView = findViewById(R.id.clickPoints);
+            MainActivity.clickPointString= Integer.toString(MainActivity.clickPointCounter);
+            textView.setText(MainActivity.clickPointString);
+        }
 
     }
+
 }
